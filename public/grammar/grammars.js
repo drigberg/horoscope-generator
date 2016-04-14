354add4b2ad804a381e0c2ceb230cae4d43841ed
Dan At Work <daniel.rigberg@gmail.com>
2016-04-14 15:58:33 -0400

    implement more tags and better tag propagation

--- public/grammar/grammars.js
+++ public/grammar/grammars.js
@@ -1,174 +1,160 @@
 var sentence_types = {
-	"sign_declaration" : {
-		"sign" : true,
-		"active" : true
+	"secondPerson_signDeclaration" : {
+		"object" : "sign",
+		"voice" : "active",
+		"tense" : "present",
+		"person" : "second"
 	} ,
-	"bad_statement" : {
-		"bad" : true,
-		"future" : true
-	}
+	"signDeclaration_thirdPerson_" : {
+		"object" : "sign" ,
+		"voice" : "active" ,
+		"tense" : "past" ,
+		"person" : "third"
+	} ,
+	"badStatement_secondPerson" : {
+		"good/bad" : "bad" ,
+		"tense" : "present" ,
+		"person" : "second",
+		"object" : "role" ,
+		"modified_object" : true
+	} ,
+	"badStatement_thirdPerson" : {
+		"good/bad" : "bad" ,
+		"tense" : "present" ,
+		"person" : "third" ,
+		"object" : "role" ,
+		"modified_object" : true
+	} 
 }
 
-var grammars = {
-	general_grammar : {
-		"@ROOT" : {
-			"@Subject @VerbPhrase"  : {"weight": 4, "bad" : true},
-			"@PrepositionalPhrase , @Subject @VerbPhrase" : {"weight": 4}
-		} ,
-		"@VerbPhrase" : {
-			"@Verb @Object" : {"weight": 4}
-		} ,
-		"@Verb" : {
-			"eaten" : {
-				"weight": 4,
-				"passive": true,
-				"bad": true
-			} ,
-			"destroyed" : {
-				"weight": 4,
-				"passive": true,
-				"bad": true
-			} ,
-			"destroy" : {
-				"weight": 4,
-				"active": true,
-				"bad": true
-			} ,
-			"subjugated" : {
-				"weight": 4,
-				"active": true,
-				"bad": true
-			} ,
-			"philosophically challenged" : {
-				"weight": 4,
-				"passive": true,
-				"bad": true
-			} ,
-			"granted diplomatic immunity" : {
-				"weight": 4,
-				"passive": true,
-				"bad": true
-			} ,
-			"sacrificed" : {
-				"weight": 4,
-				"passive": true,
-				"bad": true
-			} ,
-	 		"metaphorically conquer" : {
-				"weight": 4,
-				"active": true,
-				"bad": true
-			} ,
-	 		"assert your dominance over" : {
-				"weight": 4,
-				"active": true,
-				"bad": true
-			}
-		} , 
-		"@PrepositionalPhrase" : {
-			"@Preposition @Object" : {"weight" : 4}
-		} ,
-		"@Subject" : {
-			"you" : {
-				"weight": 4,
-				"person": "second"
-			} ,
-			"your mother" : {
-				"weight": 4,
-				"person": "third"
-			} ,
-			"your father" : {
-				"weight": 4,
-				"person": "third"
-			} ,
-		} ,
-		"@Object" : {
-			"@Object @PrepositionalPhrase" : {"weight": 1} ,
-			"@ProperNoun" : {"weight": 7} ,
-			"@Conjunction @Noun" : {"weight": 7}
-		} ,
-		"@Preposition" : {
-			"in" : {"weight": 4},
-			"with" : {"weight": 4},
-			"thanks to" : {"weight": 4}
-		} ,
-		"@Noun" : {
-			"star" : {"weight": 4},
-			"galaxy" : {"weight": 4},
-			"moose" : {"weight": 4},
-			"Aries" : {
-				"weight" : 4,
-				"sign" : true
-			}
-		} ,
-		"@ProperNoun" : {
-			"Master Commander" : {"weight": 4},
-			"Lord Ruler" : {"weight": 4},
-			"God" : {"weight": 4},
-			"Joe" : {"weight": 4}
-		} ,
-		"@Conjunction" : {
-			"the" : {"weight": 4},
-			"a" : {"weight": 4}
+var grammar = {
+	"@ROOT" : {
+		"@Subject @VerbPhrase"  : {
+			"weight": 4
 		}
 	} ,
-	old_grammar : {
-		"@ROOT" : {
-			"OLD @Subject @PassivePredictor" : {"weight" : 4} , 
-			"OLD @Subject @ActivePredictor"  : {"weight": 4} ,
-			"OLD @Subject @ActiveStatement" : {"weight": 4} ,
-			"OLD @Subject @PassiveHistory" : {"weight": 4} ,
-		} ,
-		"@PassivePredictor" : {
-			"will be @PassiveVerb @PrepositionalPhrase" : {"weight": 4}
-		} ,
-		"@ActivePredictor" : {
-			"will @ActiveVerb @Object" : {"weight": 4}
-		} ,
-		"@ActiveStatement" : {
-			"@ActiveVerb @Object" : {"weight": 4}
-		} , 
-		"@PassiveHistory" : {
-			"have been @PassiveVerb @PrepositionalPhrase" : {"weight": 4}
-		} , 
-		"@PassiveVerb" : {
-			"eaten" : {"weight": 4},
-			"destroyed" : {"weight": 4},
-			"subjugated" : {"weight": 4},
-			"philosophically challenged" : {"weight": 4}
-		} , 
-		"@ActiveVerb" : {
-	 		"metaphorically conquer" : {"weight": 4},
-	 		"assert your dominance over" : {"weight" : 4}
-		} ,
-		"@PrepositionalPhrase" : {
-			"in the @Object" : {"weight" : 4}
-		} ,
-		"@Subject" : {
-			"you" : {"weight": 4} ,
-			"your mother" : {"weight": 4} ,
-			"your father" : {"weight": 4} ,
-		} ,
-		"@Object" : {
-			"@Object @PrepositionalPhrase" : {"weight": 1} ,
-			"@ProperNoun" : {"weight": 7} ,
-			"@Conjunction @Noun" : {"weight": 7}
-		} ,
-		"@Noun" : {
-			"star" : {"weight": 4},
-			"galaxy" : {"weight": 4},
-			"moose" : {"weight": 4}
-		} ,
-		"@ProperNoun" : {
-			"Master Commander" : {"weight": 4},
-			"Lord Ruler" : {"weight": 4},
-			"God" : {"weight": 4},
-			"Joe" : {"weight": 4}
-		} ,
-		"@Conjunction" : {
-			"the" : {"weight": 4},
-			"a" : {"weight": 4}
+	"@VerbPhrase" : {
+		"@Verb @Object" : {
+			"weight": 4
 		}
+	} ,
+	"@Object" : {
+		// "@Object @PrepositionalPhrase" : {"weight": 1} ,
+		"@ProperNoun" : {
+			"weight": 7,
+			"object" : "name"
+		} ,
+		"@Conjunction @Noun" : {
+			"weight": 7 ,
+			"modified_object" : false
+		} ,
+		"@Conjunction @Adjective @Noun" : {
+			"weight" : 2 ,
+			"modified_object" : true
+		}
+	} ,
+	"@Verb" : {
+		"are" : {
+			"weight": 4,
+			"tense" : "present",
+			"voice" : "active",
+			"person" : "second"
+		} ,
+		"were" : {
+			"weight": 4,
+			"tense" : "past",
+			"voice" : "active",
+			"person" : "second"			
+		} ,
+		"was" : {
+			"weight": 4,
+			"tense" : "past",
+			"voice" : "active",
+			"person" : "third"			
+		} ,
+		"is" : {
+			"weight": 4,
+			"tense" : "present",
+			"voice" : "active",
+			"person" : "third"			
+		}
+	} , 
+	"@PrepositionalPhrase" : {
+		"@Preposition @Object" : {"weight" : 4}
+	} ,
+	"@Subject" : {
+		"you" : {
+			"weight": 4,
+			"person": "second"
+		} ,
+		"your mother" : {
+			"weight": 4,
+			"person": "third"
+		} ,
+		"your father" : {
+			"weight": 4,
+			"person": "third"
+		}
+	} ,
+	"@Preposition" : {
+		"in" : {"weight": 4},
+		"with" : {"weight": 4},
+		"thanks to" : {"weight": 4}
+	} ,
+	"@Noun" : {
+		"star" : {
+			"weight": 4,
+			"object" : "thing"
+		},
+		"moose" : {
+			"weight": 4,
+			"object" : "thing"
+		} ,
+		"Aries" : {
+			"weight" : 4,
+			"object" : "sign"
+		} ,
+		"Gemini" : {
+			"weight" : 4,
+			"object" : "sign"
+		} ,
+		"athlete" : {
+			"weight" : 4,
+			"object" : "role"
+		} ,
+		"scientist" : {
+			"weight" : 4,
+			"object" : "role"
+		} 
+	} ,
+	"@ProperNoun" : {
+		"Master Commander" : {"weight": 4},
+		"Lord Ruler" : {"weight": 4},
+		"God" : {"weight": 4},
+		"Joe" : {"weight": 4}
+	} ,
+	"@Conjunction" : {
+		"a" : {
+			"weight": 4
+		}
+	} ,
+	"@Adjective" : {
+		"wonderful" : {
+			"weight" : 4,
+			"good/bad" : "good"
+		} ,
+		"amazing" : {
+			"weight" : 4,
+			"good/bad" : "good"
+		} ,
+		"terrible" : {
+			"weight" : 4,
+			"good/bad" : "bad"
+		} ,
+		"horrible" : {
+			"weight" : 4,
+			"good/bad" : "bad"
+		} 				
 	}
 }
 
--- public/js/sentenceGenerator.js
+++ public/js/sentenceGenerator.js
@@ -8,7 +8,6 @@ var paragraph = [];
 generateButton.addEventListener("click", function(){
     //generate list of sentences
     //for each sentence type, generate sentence and append to paragraph
-    var active_grammar = grammars[Object.keys(grammars)[Math.floor(Math.random()*Object.keys(grammars).length)]]
     sentence = {
         "content" : ["@ROOT"],
         "complete" : false,
@@ -23,7 +22,6 @@ generateButton.addEventListener("click", function(){
 
 function generateSentence(){
     //var active_grammar = grammars[Object.keys(grammars)[Math.floor(Math.random()*Object.keys(grammars).length)]]
-    var active_grammar = grammars["general_grammar"];
     sentence.complete = false;
     //var tripwire = 0;
     //convert nonterminals until only terminals are left
@@ -32,22 +30,24 @@ function generateSentence(){
         sentence.complete = true;
         //console.log("Current sentence is: " + sentence.content);
         for (var index = 0; index < sentence.content.length; index++){
-            if (sentence.content[index] in active_grammar){
+            if (sentence.content[index] in grammar){
                 var followingList = [];
                 var convertedTextList = [];
                 sentence.complete = false;
-                for (following in active_grammar[sentence.content[index]]){
+                for (following in grammar[sentence.content[index]]){
                     var testForAgreement = true;
                     for (tag in sentence.tags) {
-                        if (tag in active_grammar[sentence.content[index]][following]) {
-                            if (active_grammar[sentence.content[index]][following][tag] !== sentence.tags[tag]) {
+                        if (tag in grammar[sentence.content[index]][following]) {
+                            if (grammar[sentence.content[index]][following][tag] !== sentence.tags[tag]) {
                                 testForAgreement = false;
                             }
                         }
                     }
-                    for (var freq = 0; freq < active_grammar[sentence.content[index]][following]["weight"]; freq++){
-                        followingList.push(following);
-                    }                      
+                    if (testForAgreement == true) {
+                        for (var freq = 0; freq < grammar[sentence.content[index]][following]["weight"]; freq++){
+                            followingList.push(following);
+                        } 
+                    }                     
                 }
                 var newText = followingList[Math.floor(Math.random()*followingList.length)];
                 if(newText){
