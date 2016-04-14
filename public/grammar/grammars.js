cc9032db6e7eff338fc63fad8633269e126f03ba
Dan At Work <daniel.rigberg@gmail.com>
2016-04-14 16:08:42 -0400

    fix lingering merge conflicts

--- public/grammar/grammars.js
+++ public/grammar/grammars.js
@@ -1,61 +1,3 @@
-<<<<<<< HEAD
-
-var grammar = {
-	"@ROOT" : {
-		"@Subject @VerbPhrase"  : {"weight": 4}
-	} ,
-	"@VerbPhrase" : {
-		"@Verb @Object" : {"weight": 4}
-	} ,
-	"@Verb" : {
-		"eaten" : {
-			"weight": 4,
-			"voice": "passive",
-			"type": "bad_thing"
-		} ,
-		"destroyed" : {
-			"weight": 4,
-			"voice": "passive",
-			"type": "bad_thing"
-		} ,
-		"destroy" : {
-			"weight": 4,
-			"voice": "active",
-			"type": "bad_thing"
-		} ,
-		"subjugated" : {
-			"weight": 4,
-			"voice": "active",
-			"type": "bad_thing"
-		} ,
-		"philosophically challenged" : {
-			"weight": 4,
-			"voice": "passive",
-			"type": "bad_thing"
-		} ,
-		"granted diplomatic immunity" : {
-			"weight": 4,
-			"voice": "passive",
-			"type": "good_thing"
-		} ,
-		"sacrificed" : {
-			"weight": 4,
-			"type": "bad_thing"
-		} ,
- 		"metaphorically conquer" : {
-			"weight": 4,
-			"voice": "active",
-			"type": "good_thing"
-		} ,
- 		"assert your dominance over" : {
-			"weight": 4,
-			"voice": "active",
-			"type": "good_thing"
-		} , 
-		"are" : {
-			"type" : "sign_declaration",
-			"voice" : "active" 
-=======
 var sentence_types = {
 	"secondPerson_signDeclaration" : {
 		"object" : "sign",
@@ -89,41 +31,11 @@ var grammar = {
 	"@ROOT" : {
 		"@Subject @VerbPhrase"  : {
 			"weight": 4
->>>>>>> v3.0
 		}
 	} , 
 	"@PrepositionalPhrase" : {
 		"@Preposition @Object" : {"weight" : 4}
 	} ,
-<<<<<<< HEAD
-	"@Subject" : {
-		"you" : {
-			"weight": 4,
-			"person": "second"
-		} ,
-		"your mother" : {
-			"weight": 4,
-			"person": "third"
-		} ,
-		"your father" : {
-			"weight": 4,
-			"person": "third"
-		} ,
-	} ,
-	"@Object" : {
-		"@Object @PrepositionalPhrase" : {
-			"weight": 1,
-			"type" : "not_sign_declaration"
-
-		} ,
-		"@ProperNoun" : {
-			"weight": 7,
-			"type" : "not_sign_declaration"
-		} ,
-		"@Conjunction @Noun" : {
-			"weight": 7
-		}
-=======
 	"@VerbPhrase" : {
 		"@Verb @Object" : {
 			"weight": 4
@@ -186,37 +98,14 @@ var grammar = {
 			"weight": 4,
 			"person": "third"
 		}
->>>>>>> v3.0
 	} ,
 	"@Preposition" : {
 		"in" : {"weight": 4},
 		"with" : {"weight": 4},
-<<<<<<< HEAD
-		"thanks to" : {"weight": 4},
-		"for better or for worse" : {"weight": 4} 
-=======
-		"thanks to" : {"weight": 4}
->>>>>>> v3.0
 	} ,
 	"@Noun" : {
 		"star" : {
 			"weight": 4,
-<<<<<<< HEAD
-			"type" : "not_sign_declaration"
-		},
-		"galaxy" : {
-			"weight": 4,
-			"type" : "not_sign_declaration"
-		},
-		"moose" : {
-			"weight": 4,
-			"type" : "not_sign_declaration"
-		},
-		"Aries" : {
-			"weight" : 4,
-			"type" : "sign_declaration"
-		}
-=======
 			"object" : "thing"
 		},
 		"moose" : {
@@ -239,7 +128,6 @@ var grammar = {
 			"weight" : 4,
 			"object" : "role"
 		} 
->>>>>>> v3.0
 	} ,
 	"@ProperNoun" : {
 		"Master Commander" : {"weight": 4},
@@ -248,10 +136,6 @@ var grammar = {
 		"Joe" : {"weight": 4}
 	} ,
 	"@Conjunction" : {
-<<<<<<< HEAD
-		"the" : {"weight": 4},
-		"a" : {"weight": 4}
-=======
 		"a" : {
 			"weight": 4
 		}
@@ -273,7 +157,6 @@ var grammar = {
 			"weight" : 4,
 			"good/bad" : "bad"
 		} 				
->>>>>>> v3.0
 	}
 }
 
