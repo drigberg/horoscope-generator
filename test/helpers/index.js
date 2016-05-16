8700e6f9cec2ad1d7c0f60f71f70b344067696a6
Dan At Work <daniel.rigberg@gmail.com>
2016-05-16 18:21:43 -0400

    begin implementing testing

---
+++ test/helpers/index.js
@@ -0,0 +1,5 @@
+exports.validForm = {
+  name : "Daniel",
+  hometown : "Hershey",
+  birthday : "1993-03-11"
+};
--- test/sentenceGenerator_spec.js
+++ test/sentenceGenerator_spec.js
@@ -1,3 +1,29 @@
 var assert = require("assert");
-
 var sentenceGenerator = require("../public/js/sentenceGenerator.js");
+var helpers = require("./helpers");
+
+describe("Horoscope form requirements", function(){
+  var horoscope = sentenceGenerator.horoscope;
+  before(function(){
+    validForm = helpers.validForm;
+  });
+  describe("Form valid if...", function(){
+    it("all validators successful", function(){
+      assert(validApp.isValid(), "Not valid");
+    });
+  });
+  describe("Form invalid if...", function(){
+    it("hometown is blank", function(){
+      var app = new MembershipApplication({hometown: "")});
+      assert(!app.hometownIsValid())
+    });
+    it("name is blank", function () {
+      var app = new MembershipApplication({name : ""});
+      assert(!app.nameIsValid());
+    });
+    it("birthday is incomplete", function () {
+      var app = new MembershipApplication({birthday : ""});
+      assert(!app.birthdayIsValid());
+    });
+  });
+});
