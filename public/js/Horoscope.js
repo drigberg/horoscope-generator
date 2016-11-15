31bd9d5c67f2116b3b1fae1981700ad963e6ec97
Daniel Rigberg <daniel.rigberg@gmail.com>
2016-11-15 16:27:14 -0500

    add more vocabulary, sentence types, remove name sign declaration

--- public/js/Horoscope.js
+++ public/js/Horoscope.js
@@ -10,7 +10,6 @@ var Horoscope = function(args){
     //takes in form and grammar data to generate contextually sensible horoscope
     args || (args = {});
     _.extend(this,args);
-    this.sentence_types = {};
     this.grammar = {};
     this.sentenceBigramProbabilities = {};
     this.sentenceTypes = {};
@@ -39,25 +38,25 @@ var Horoscope = function(args){
         //finds user's sign, initializes sentence
         //adds name to grammar
         this.date = moment().format("MMMM Do YYYY, h:mm a");
-        if (this.userData["name"] !== ""){
-            this.sentenceTypes["name_signDeclaration"] = {
-                "object" : "sign",
-                "voice" : "active",
-                "name" : true,
-                "tense" : "present",
-                "person" : "third" ,
-                "verbtype" : "linking"
-            };
-            this.grammar["@Subject"]["@Name"] = {
-                "weight" : 50,
-                "person" : "third" ,
-                "name" : true
-            };
-            this.grammar["@Name"] = {};
-            this.grammar["@Name"][this.userData["name"]] = {
-                "weight" : 4
-            };
-        }
+        // if (this.userData["name"] !== ""){
+        //     this.sentenceTypes["name_signDeclaration"] = {
+        //         "object" : "sign",
+        //         "voice" : "active",
+        //         "name" : true,
+        //         "tense" : "present",
+        //         "person" : "third" ,
+        //         "verbtype" : "linking"
+        //     };
+        //     this.grammar["@Subject"]["@Name"] = {
+        //         "weight" : 50,
+        //         "person" : "third" ,
+        //         "name" : true
+        //     };
+        //     this.grammar["@Name"] = {};
+        //     this.grammar["@Name"][this.userData["name"]] = {
+        //         "weight" : 4
+        //     };
+        // }
         this.userData.sign = this.evaluateSign(this.userData.birthday);
         this.grammar["@Noun"]["@Sign"] = {
             "weight" : 50 ,
