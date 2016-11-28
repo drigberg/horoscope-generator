3c415df75d21d7b1f64e3a8980daf3e35dbb2622
Daniel Rigberg <daniel.rigberg@gmail.com>
2016-11-28 18:30:02 -0500

    add all draft sign images

--- seeds.js
+++ seeds.js
@@ -5,11 +5,110 @@ var data = [
     {
         full_text       : "Your mother was a terrible scientist!!",
         abridged_text   : "Misha's mother was a terrible scientist!!",
-        image           : "http://www.romanceways.com/files/2012/03/How-to-date-a-leo.jpg",
+        image           : "assets/aquarius.jpg",
+        author          : "MISHA",
+        hometown        : "Jupiter",
+        date            : "May 12th 2016, 8:25pm",
+        sign            : "Aquarius"
+    },
+    {
+        full_text       : "Your mother was a terrible scientist!!",
+        abridged_text   : "Misha's mother was a terrible scientist!!",
+        image           : "assets/aries.png",
+        author          : "MISHA",
+        hometown        : "Jupiter",
+        date            : "May 12th 2016, 8:25pm",
+        sign            : "Aries"
+    },
+    {
+        full_text       : "Your mother was a terrible scientist!!",
+        abridged_text   : "Misha's mother was a terrible scientist!!",
+        image           : "assets/capricorn.png",
+        author          : "MISHA",
+        hometown        : "Jupiter",
+        date            : "May 12th 2016, 8:25pm",
+        sign            : "Cancer"
+    },
+    {
+        full_text       : "Your mother was a terrible scientist!!",
+        abridged_text   : "Misha's mother was a terrible scientist!!",
+        image           : "assets/pisces.jpg",
+        author          : "MISHA",
+        hometown        : "Jupiter",
+        date            : "May 12th 2016, 8:25pm",
+        sign            : "Capricorn"
+    },
+    {
+        full_text       : "Your mother was a terrible scientist!!",
+        abridged_text   : "Misha's mother was a terrible scientist!!",
+        image           : "assets/taurus.jpg",
         author          : "MISHA",
         hometown        : "Jupiter",
         date            : "May 12th 2016, 8:25pm",
         sign            : "Gemini"
+    },
+    {
+        full_text       : "Your mother was a terrible scientist!!",
+        abridged_text   : "Misha's mother was a terrible scientist!!",
+        image           : "assets/gemini.jpg",
+        author          : "MISHA",
+        hometown        : "Jupiter",
+        date            : "May 12th 2016, 8:25pm",
+        sign            : "Leo"
+    },
+    {
+        full_text       : "Your mother was a terrible scientist!!",
+        abridged_text   : "Misha's mother was a terrible scientist!!",
+        image           : "assets/cancer.png",
+        author          : "MISHA",
+        hometown        : "Jupiter",
+        date            : "May 12th 2016, 8:25pm",
+        sign            : "Libra"
+    },
+    {
+        full_text       : "Your mother was a terrible scientist!!",
+        abridged_text   : "Misha's mother was a terrible scientist!!",
+        image           : "assets/leo.png",
+        author          : "MISHA",
+        hometown        : "Jupiter",
+        date            : "May 12th 2016, 8:25pm",
+        sign            : "Pisces"
+    },
+    {
+        full_text       : "Your mother was a terrible scientist!!",
+        abridged_text   : "Misha's mother was a terrible scientist!!",
+        image           : "assets/virgo.png",
+        author          : "MISHA",
+        hometown        : "Jupiter",
+        date            : "May 12th 2016, 8:25pm",
+        sign            : "Sagittarius"
+    },
+    {
+        full_text       : "Your mother was a terrible scientist!!",
+        abridged_text   : "Misha's mother was a terrible scientist!!",
+        image           : "assets/libra.jpg",
+        author          : "MISHA",
+        hometown        : "Jupiter",
+        date            : "May 12th 2016, 8:25pm",
+        sign            : "Scorpius"
+    },
+    {
+        full_text       : "Your mother was a terrible scientist!!",
+        abridged_text   : "Misha's mother was a terrible scientist!!",
+        image           : "assets/scorpius.png",
+        author          : "MISHA",
+        hometown        : "Jupiter",
+        date            : "May 12th 2016, 8:25pm",
+        sign            : "Taurus"
+    },
+    {
+        full_text       : "Your mother was a terrible scientist!!",
+        abridged_text   : "Misha's mother was a terrible scientist!!",
+        image           : "assets/sagittarius.jpg",
+        author          : "MISHA",
+        hometown        : "Jupiter",
+        date            : "May 12th 2016, 8:25pm",
+        sign            : "Virgo"
     }
 ]
 
