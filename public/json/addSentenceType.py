4ab08bbbef67ed09cb5adb86cef993a657a71486
Daniel Rigberg <daniel.rigberg@gmail.com>
2016-12-05 14:33:21 -0500

    complete draft

--- public/json/addSentenceType.py
+++ public/json/addSentenceType.py
@@ -9,31 +9,41 @@ def main():
         "signDeclaration_secondPerson" : 0,
         "@END"                         : 0.3
     }
-    with open('sentenceTypes.json') as sentence_types_data:
-        with open('sentenceBigrams.json') as bigrams_data:
+    sentence_types_file = 'sentenceTypes.json'
+    bigram_file = 'sentenceBigrams.json'
+    with open(sentence_types_file) as sentence_types_data:
+        with open(bigram_file) as bigrams_data:
+            operation = raw_input("Add or remove? (a/r): ")
+            if operation.lower() == "a":
+                sentence_types = json.load(sentence_types_data)
+                bigrams = json.load(bigrams_data)
+                title = raw_input("Sentence type name: ")
+                num_tags = input("Number of tags to add: ")
+                tags = {}
+                for n in range(int(num_tags)):
+                    print "\n******"
+                    new_tag = raw_input("New tag name: ")
+                    tag_value = raw_input("Tag value: ")
+                    print "******\n"
+                    tags[new_tag] = tag_value
+                sentence_types[title] = tags
 
-            sentence_types = json.load(sentence_types_data)
-            bigrams = json.load(bigrams_data)
-            counter = 0
-            breaker = ""
-            title = raw_input("Sentence type name: ")
-            num_tags = input("Number of tags to add: ")
-            tags = {}
-            for n in range(int(num_tags)):
-                print "******"
-                new_tag = raw_input("New tag name: ")
-                tag_value = raw_input("Tag value: ")
-                print "******"
-                tags[new_tag] = tag_value
+            elif operation.lower() == "r":
+                sentence_types = json.load(sentence_types_data)
+                bigrams = json.load(bigrams_data)
+                title = raw_input("Sentence type to remove: ")
+                sentence_types.pop(title, None)
+                bigrams.pop(title, None)
+                for sentence_type in bigrams:
+                    bigrams[sentence_type].pop(title, None)
+                print "%s removed from %s and %s \n" % (title, sentence_types_file, bigram_file)
 
-            sentence_types[title] = tags
+    print "New sentence type list: "
+    for key in sentence_types:
+        print "\t %s" % key
 
-            #add sentence type as conversion for each existing type and as node
-
-
-
-
-
+    print "*********\nCleaning bigrams...."
+    bigrams = cleanBigrams.cleanProbabilities(bigrams)
 
     # with open('sentence_types.json', 'w') as revised_sentence_types_file:
     #     json.dump(sentence_types, revised_sentence_types_file)
