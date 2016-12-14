3fe5590eeb49fc8407fbe19bc4e0c37cb073ff7e
Daniel Rigberg <daniel.rigberg@gmail.com>
2016-12-14 13:22:49 -0500

    restyle JS

--- test/Horoscope_spec.js
+++ test/Horoscope_spec.js
@@ -1,77 +1,77 @@
-var assert                        = require("assert");
-var helpers                       = require("./helpers");
-var calendar                      = require("../public/json/calendar.json");
-var grammar                       = require("../public/json/grammar.json");
-var sentenceBigramProbabilities   = require("../public/json/sentenceBigrams.json");
-var sentenceTypes                 = require("../public/json/sentenceTypes.json");
-var Horoscope                     = require("../public/js/Horoscope.js");
+var assert                        = require('assert');
+var helpers                       = require('./helpers');
+var calendar                      = require('../public/json/calendar.json');
+var grammar                       = require('../public/json/grammar.json');
+var sentenceBigramProbabilities   = require('../public/json/sentenceBigrams.json');
+var sentenceTypes                 = require('../public/json/sentenceTypes.json');
+var horoscope                     = require('../public/js/Horoscope.js');
 
-//set up validation functions
-var horoscope = Horoscope.Horoscope;
+// set up validation functions
+var Horoscope = horoscope.Horoscope;
 
-describe("Horoscope form requirements", function(){
-    describe("Form valid if...", function(){
-        it("all validators successful", function(){
-            var validForm = new horoscope();
+describe('Horoscope form requirements', function() {
+    describe('Form valid if...', function() {
+        it('all validators successful', function() {
+            var validForm = new Horoscope();
             validForm.userData = helpers.validForm;
-            assert(validForm.validation.validateForm(), "At least one validator unsuccessful");
+            assert(validForm.validation.validateForm(), 'At least one validator unsuccessful');
         });
     });
 
-    describe("Form invalid if...", function(){
-        it("hometown is blank", function(){
-            var invalidForm = new horoscope();
+    describe('Form invalid if...', function() {
+        it('hometown is blank', function() {
+            var invalidForm = new Horoscope();
             assert(!invalidForm.validation.hometownIsValid())
         });
-        it("name is blank", function () {
-            var invalidForm = new horoscope();
+        it('name is blank', function() {
+            var invalidForm = new Horoscope();
             assert(!invalidForm.validation.nameIsValid());
         });
-        it("birthday is incomplete", function () {
-            var invalidForm = new horoscope();
+        it('birthday is incomplete', function() {
+            var invalidForm = new Horoscope();
             assert(!invalidForm.validation.birthdayIsValid());
         });
     });
 });
 
-describe("Sign Evaluation", function(){
-    describe("Evaluation correct if...", function(){
-        it("All months split correctly", function(){
-            var all_correct = true;
-            for (i = 0; i < helpers.correctSigns.length; i++){
-                var date = "2000-" + Object.keys(helpers.correctSigns[i])[0];
-                var user = new horoscope();
+describe('Sign Evaluation', function() {
+    describe('Evaluation correct if...', function() {
+        it('All months split correctly', function() {
+            var allCorrect = true;
+            for (i = 0; i < helpers.correctSigns.length; i++) {
+                var date = '2000-' + Object.keys(helpers.correctSigns[i])[0];
+                var user = new Horoscope();
                 user.userData.birthday = date;
                 user.calendar = calendar;
-                if (user.evaluateSign(user.userData.birthday) != helpers.correctSigns[i][Object.keys(helpers.correctSigns[i])[0]]){
-                    all_correct = false;
+                if (user.evaluateSign(user.userData.birthday) != helpers.correctSigns[i][Object.keys(helpers.correctSigns[i])[0]]) {
+                    allCorrect = false;
                 }
             }
-            assert(all_correct, "Sign mismatch");
+            assert(allCorrect, 'Sign mismatch');
         });
     });
 
-    describe("Evaluation incorrect if...", function(){
-        it("Months split noninclusively along divide", function(){
-            var all_incorrect = true;
-            for (i = 0; i < helpers.incorrectSigns.length; i++){
-                var date = "2000-" + Object.keys(helpers.incorrectSigns[i])[0];
-                var user = new horoscope({userData : {birthday : date}, calendar : calendar});
-                if (user.evaluateSign(user.userData.birthday) == helpers.incorrectSigns[i][Object.keys(helpers.incorrectSigns[i])[0]]){
-                    all_incorrect = false;
+    describe('Evaluation incorrect if...', function() {
+        it('Months split noninclusively along divide', function() {
+            var allIncorrect = true;
+            for (i = 0; i < helpers.incorrectSigns.length; i++) {
+                var date = '2000-' + Object.keys(helpers.incorrectSigns[i])[0];
+                var user = new Horoscope( {userData :  {birthday : date}, calendar : calendar});
+                if (user.evaluateSign(user.userData.birthday) == helpers.incorrectSigns[i][Object.keys(helpers.incorrectSigns[i])[0]]) {
+                    allIncorrect = false;
                 }
             }
-            assert(all_incorrect, "Splits inclusively along divide within months");
+            assert(allIncorrect, 'Splits inclusively along divide within months');
         });
     });
 });
 
-describe("Sentences", function(){
-    describe("Sentence valid if...", function(){
-        validSentence = new horoscope({
-            sentenceContainsOnlyTerminals : function(sentence){
-                for (var n = 0; n < sentence.content.length; n++){
-                    if (sentence.content[n].indexOf("@") > -1){
+describe('Sentences', function() {
+    describe('Sentence valid if...', function() {
+        validSentence = new Horoscope( {
+            sentenceContainsOnlyTerminals: function(sentence) {
+                for (var n = 0; n < sentence.content.length; n++) {
+                    if (sentence.content[n].indexOf('@') > -1) {
                         return false;
                     }
                 }
@@ -86,42 +86,42 @@ describe("Sentences", function(){
         validSentence.userData.sign = validSentence.evaluateSign(validSentence.userData.birthday);
         validSentence.addUserDataToGrammar();
 
-        var sentence_types_list = [];
-        for (var i = 0; i < Object.keys(sentenceTypes).length; i++) {
-            sentence_types_list.push(Object.keys(sentenceTypes)[i]);
+        var sentenceTypesList = [];
+        for (var i = 0; i < Object.keys(sentenceTypes).length; i++)  {
+            sentenceTypesList.push(Object.keys(sentenceTypes)[i]);
         };
 
         var allValid = true;
         for (var n = 0; n < 10000; n++) {
-            sentence_type = sentence_types_list[Math.floor(Math.random() * sentence_types_list.length)]
-            newSentence = validSentence.generateSentence(sentence_type);
-            if (!(validSentence.sentenceContainsOnlyTerminals(newSentence))){
+            sentenceType = sentenceTypesList[Math.floor(Math.random() * sentenceTypesList.length)]
+            newSentence = validSentence.generateSentence(sentenceType);
+            if (!(validSentence.sentenceContainsOnlyTerminals(newSentence))) {
                 allValid = false;
                 break;
             };
         }
 
-        it("it contains only terminals", function(){
-            assert(allValid, "Generator can output nonterminals");
+        it('it contains only terminals', function() {
+            assert(allValid, 'Generator can output nonterminals');
         });
     });
 });
 
-describe("Paragraphs", function(){
-    describe("Structure valid if...", function(){
-        correctStructure = new horoscope({
-            structureContainsOnlyBigramElements : function(){
-                for (var n = 0; n < that.structure.length - 1; n++) {
-                    if (!(horoscope.structure[n] in horoscope.sentenceBigramProbabilities || horoscope.structure[n] == "@END") && (horoscope.structure[n + 1] in horoscope.sentenceBigramProbabilities || horoscope.structure[n + 1] == "@END")){
+describe('Paragraphs', function() {
+    describe('Structure valid if...', function() {
+        correctStructure = new Horoscope( {
+            structureContainsOnlyBigramElements: function() {
+                for (var n = 0; n < that.structure.length - 1; n++)  {
+                    if (!(horoscope.structure[n] in horoscope.sentenceBigramProbabilities || horoscope.structure[n] == '@END') && (horoscope.structure[n + 1] in horoscope.sentenceBigramProbabilities || horoscope.structure[n + 1] == '@END')) {
                         return false;
                     }
                 }
                 return true;
             },
-            structureContainsOnlyNonzeroBigrams : function(){
-                if (this.structureContainsOnlyBigramElements()){
+            structureContainsOnlyNonzeroBigrams: function() {
+                if (this.structureContainsOnlyBigramElements()) {
                     for (var n = 0; n < that.structure.length - 1; n++) {
-                        if (that.sentenceBigramProbabilities[that.structure[n]][that.structure[n+1]] == 0) {
+                        if (that.sentenceBigramProbabilities[that.structure[n]][that.structure[n+1]] == 0)  {
                             return false;
                         }
                     }
@@ -132,17 +132,17 @@ describe("Paragraphs", function(){
             }
         });
         correctStructure.structure = helpers.correctStructure;
-        it("it begins with @START", function(){
-            assert(correctStructure.structure.indexOf("@START") == 0, "structure does not begin with @START");
+        it('it begins with @START', function() {
+            assert(correctStructure.structure.indexOf('@START') == 0, 'structure does not begin with @START');
         });
-        it("it ends with @END", function(){
-            assert(correctStructure.structure.indexOf("@END") == correctStructure.structure.length - 1, "structure does not end with @END");
+        it('it ends with @END', function() {
+            assert(correctStructure.structure.indexOf('@END') == correctStructure.structure.length - 1, 'structure does not end with @END');
         });
-        it("it only contains elements from the bigram probabilities", function(){
-            assert(correctStructure.structureContainsOnlyBigramElements(), "structure contains foreign elements");
+        it('it only contains elements from the bigram probabilities', function() {
+            assert(correctStructure.structureContainsOnlyBigramElements(), 'structure contains foreign elements');
         });
-        it("it only contains nonzero bigrams", function(){
-            assert(correctStructure.structureContainsOnlyNonzeroBigrams(), "structure contains zero-probability bigrams");
+        it('it only contains nonzero bigrams', function() {
+            assert(correctStructure.structureContainsOnlyNonzeroBigrams(), 'structure contains zero-probability bigrams');
         });
     });
 });
